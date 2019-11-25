183c63d4fc4ddb292cd934b969a139bd9bb8f27c
jdickson0296 <jdickson0296@gmail.com>
2019-11-24 21:34:36 -0800

    aws code for pcap to csv conversion

--- project_code.py
+++ project_code.py
@@ -2,6 +2,8 @@ import geoip2.database
 import dpkt
 import socket
 import pandas as pd
+import boto3
+import json
 
 def readPcap(pcap_file):
     """
@@ -86,13 +88,54 @@ def ip_to_csv(pcapFile):
         country = geo_country(x)
         # filters for ip's that have at least a city or country location
         if city != "None" and country != "None":
-            scr_ip.append(x)
-            city_list.append(city)
-            country_list.append(country)
+            scr_ip.append(str(x))
+            city_list.append(str(city))
+            country_list.append(str(country))
         else:
             pass
     # writes the data to a csv file
     df = pd.DataFrame({'IP' : scr_ip, 'City' : city_list, 'Country' : country_list})
     df.to_csv('IP_GeoLocation.csv', encoding='utf-8', index=False)
+
+
+def pcap_to_csv(event, context):
+    # **generate variables for S3 and DynamoDB clients**
+    s3 = boto3.client('s3')
+    dynamodb = boto3.client('dynamodb')
+    # **get the bucket name and pcap data file name as key**
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    # **Dont process if the files does not have a .pcap extn**
+    if '.pcap' not in key:
+        return 'Please upload .csv files only.'
+    # **download the .pcap file to /tmp folder**
+    s3.download_file(bucket, key, '/tmp/' + key)
+    pcapName = 'processed_' + key[0:-4] + '.pcap'
+    # Open the .pcap file to process it, and upload the processed .csv file
+    ip_to_csv(pcapName)
+    csvName = 'IP_GeoLocation.csv'
+    s3.upload_file('/tmp/' + csvName, bucket, csvName)
+    # Use the DynamoDB atomic counters to add/update data in the DynamoDB
+    with open('/tmp/' + key, 'r') as infile:
+        first_line = infile.readline()
+        for row in infile:
+            ddb_IP = row.strip().split(',')[0]
+            ddb_City = row.strip().split(',')[1]
+            ddb_Country = row.strip().split(',')[2]
+            response = dynamodb.update_item(
+                TableName='209-logs',
+                Key={
+                    'source-ip': {'S': ddb_IP},
+                    'country': {'S': ddb_Country},
+                },
+                UpdateExpression='ADD city :city',
+                ExpressionAttributeValues={
+                    ':city': {'S': ddb_City}
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            print(response)
+
+
 
 ip_to_csv('smallFlows.pcap')
